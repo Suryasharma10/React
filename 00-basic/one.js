de7541b5dd455ff86082e0f606b3e1fa709c3168
Surya Sharma <suryasharma73077@gmail.com>
2026-03-14 14:07:45 +0530

    Add files via upload

---
+++ 00-basic/one.js
@@ -0,0 +1,16 @@
+var div = document.createElement('div')
+var h1 = document.createElement('h1')
+h1.innerHTML="Good Morning everone";
+var p = document.createElement('p')
+p.innerHTML = "My name is surya pratap sharma"
+div.style.backgroundColor ="Green";
+div.style.borderRadius ="50%";
+div.style.height="23rem";
+div.style.width="23rem";
+p.style.color ="white"
+h1.style.color ="white"
+document.body.appendChild(div)
+document.body.appendChild(h1)
+document.body.appendChild(p)
+console.log(h1)
+console.log(p)
---
+++ 00-basic/two.js
@@ -0,0 +1,10 @@
+// import name from "./three.js"
+// console.log(name);
+// import age from "./three.js"
+// console.log(age);
+import name,{age,arr,Surya}from './three.js'
+console.log(age);
+console.log(name);
+console.log(arr.length);
+console.log(arr);
+Surya();
